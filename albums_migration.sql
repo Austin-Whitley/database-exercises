a38f6a124709fc018f1562c13b743c60e28f04b0
Austin Whitley <whitley.austin01@gmail.com>
2021-06-17 15:23:11 -0500

    sheesh

---
+++ albums_migration.sql
@@ -0,0 +1,11 @@
+USE codeup_test_db;
+DROP TABLE IF EXISTS albums;
+CREATE TABLE albums(
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    artist VARCHAR(50),
+    name VARCHAR(50),
+    release_date INT unsigned NOT NULL,
+    sales DOUBLE UNSIGNED NOT NULL,
+    genre VARCHAR(25),
+    PRIMARY KEY (id);
+)
